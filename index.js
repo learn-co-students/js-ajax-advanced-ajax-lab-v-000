59e90092b80b0b49db08a5cebe76ca39e3ba3cd7
Dalia <daliasawaya@tests-MacBook-Air-2.local>
2017-12-11 16:09:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  Handlebars.registerPartial("authorPartial", document.getElementById("author-partial-template").innerHTML)
+});
+
+
 function getRepositories() {
   const req = new XMLHttpRequest()
   req.addEventListener("load", showRepositories);
