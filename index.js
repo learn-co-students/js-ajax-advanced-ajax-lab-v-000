a61b79ff78f83f624d561ba192499e883399990e
Alli Tzadok <allisontzadok@gmail.com>
2018-05-30 16:40:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  Handlebars.registerPartial("authorPartial", document.getElementById("author-partial-template").innerHTML)
+});
+
+
 function getRepositories() {
   const req = new XMLHttpRequest()
   req.addEventListener("load", showRepositories)
   req.open("GET", 'https://api.github.com/users/octocat/repos')
   req.send()
 }
-function showRepositories(event, data) {
-  const repos = JSON.parse(this.responseText)
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`
-  document.getElementById("repositories").innerHTML = repoList
-}
+
 function showRepositories(event, data) {
   const repos = JSON.parse(this.responseText)
   const src = document.getElementById("repository-template").innerHTML
