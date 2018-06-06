1fcbc4ef6cacd882dbc4bbcf8ae5712c85ac1583
Robert Hughes <roberts.ginger.email@gmail.com>
2018-06-06 10:46:24 +0100

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -6,12 +6,6 @@ const path = require('path')
 
 
 describe('index', () => {
-  
-  jsdom({
-    src: fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
-  })
-  
-
   it('runs', () => {
     expect(true).toEqual(true)
   })
