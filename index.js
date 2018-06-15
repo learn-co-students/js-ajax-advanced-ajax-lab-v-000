e26d1c7b47535aa6d623b4d49f2f95e2b23a5524
github username <github email address>
2018-06-14 22:12:21 -0400

    Add author-partial-template.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function getRepositories() {
 function showRepositories(event, data) {
     const repos = JSON.parse(this.responseText)
     const src = document.getElementById("repository-template").innerHTML
+    Handlebars.registerPartial("authorPartial", document.getElementById("author-partial-template").innerHTML)
     const template = Handlebars.compile(src)
     const repoList = template(repos)
     document.getElementById("repositories").innerHTML = repoList;
