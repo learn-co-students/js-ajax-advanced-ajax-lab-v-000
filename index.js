b82bcbdba622bc10d3a55437f1006868230a97f1
coryjbergquist <cory.j.bergquist@gmail.com>
2018-09-01 18:41:22 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+  Handlebars.registerPartial(
+    'authorPartial',
+    document.getElementById('author-partial-template').innerHTML
+  );
+});
+
+
 function getRepositories() {
   const req = new XMLHttpRequest();
   req.addEventListener('load', showRepositories);
