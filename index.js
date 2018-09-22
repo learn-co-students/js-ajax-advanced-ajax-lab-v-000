8b3e59d9d207f9c6ddf5e805ba1fa91ed2166907
Zac Willmington <zac.willmington@gmail.com>
2018-09-22 23:47:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function showRepositories(event, data) {
+  const repos = JSON.parse(this.responseText);
+  const src = document.getElementById('respository-template').innerHTML;
+  const template = Handlebars.compile(src);
+  const repoList  = template(repos);
+  document.getElementById('repositories').innerHTML = repoList;
+}
+
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
