43c8437e4f542980d40cc6bf6568707332c568b4
swgoodman <spencerwgoodman@icloud.com>
2018-09-26 12:49:02 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,6 @@
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
