84cf2fe171b0982292faff5f938d87d8e040aff9
sgibson47 <sgibson47@gmail.com>
2018-10-22 11:12:21 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
+
+function showRepositories(event, data) {
+  const repos = JSON.parse(this.responseText);
+  const repoList = `<ul>${repos
+    .map(r => '<li>' + r.name + '</li>')
+    .join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = repoList;
+}
