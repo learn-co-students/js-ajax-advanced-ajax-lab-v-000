f27c824c1393a9fc2429c713e06106892b76e497
eyi1 <ej7729@gmail.com>
2018-10-29 20:48:07 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function getRepositories(){
+    const req = new XMLHttpRequest();
+    req.addEventListener('load', showRespositories);
+    req.open('Get', 'https://api.github.com/users/octocat/repos');
+    req.send();
+}
+
+function showRespositories(event, data){
+    const repos = JSON.parse(this.responseText);
+    const src = 
+        document.getElementById('repository-template').innerHTL;
+        const template = Handlebars.compile(src);
+        const repoList = template(repos);
+        
+
+    document.getElementById('repositories').innerHTML = repoList;
+}
+
+document.addEventListener('DOMcontentLoaded', 
+    function(event){
+        Handlebars.registerPartial(
+            'authorPartial',
+            document.getElementById('author-partial-template').innerHTLML
+        );
+})
