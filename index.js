01533b60f53be5c36f477ba5e42049d0dd647fc0
Carol Greene <carolgreene3@gmail.com>
2018-11-24 23:50:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-document.addEventListener('DOMContentLoaded',
-function(event) {
+document.addEventListener('DOMContentLoaded', function(event) {
   Handlebars.registerPartial(
     'authorPartial',
     document.getElementById('author-partial-template').innerHTML
@@ -10,9 +9,8 @@ function(event) {
 function getRepositories() {
   const req = new XMLHttpRequest();
   req.addEventListener('load', showRepositories);
-  req.open('GET',
-'https://api.github.com/users/octocat/repos');
-req.send();
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
 }
 
 function showRepositories(event, data) {
@@ -20,5 +18,6 @@ function showRepositories(event, data) {
   const src = document.getElementById('repository-template').innerHTML;
   const template = Handlebars.compile(src);
   const repoList = template(repos);
+
 document.getElementById('repositories').innerHTML = repoList;
 }
