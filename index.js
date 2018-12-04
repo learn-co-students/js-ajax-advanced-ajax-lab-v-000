e60a0cab2340c1364c7b2538ae35c25c62319801
stazman <cadistasio@gmail.com>
2018-12-03 20:23:48 -0600

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ function getRepositories() {
     req.open('GET', 'https://api.github.com/users/octocat/repos');
     req.send();
 }
+
+function showRepositories(event, data) {
+    const repos = JSON.parse(this.responseText);
+    const repoList = `<ul>${repos
+      .map(r => '<li>' + r.name + '</li>')
+      .join('')}</ul>`;
+    document.getElementById('repositories').innerHTML = repoList;
+}
