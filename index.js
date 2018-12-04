248df522f2007ecd650fa6a3b5884aab6d15eecf
stazman <cadistasio@gmail.com>
2018-12-03 20:25:29 -0600

    Done.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function getRepositories() {
 function showRepositories(event, data) {
     const repos = JSON.parse(this.responseText);
     const repoList = `<ul>${repos
-      .map(r => '<li>' + r.name + '</li>')
-      .join('')}</ul>`;
+        .map(r => '<li><a href="' + r.html_url + '">' + r.name + '</a></li>')
+        .join('')}</ul>`;
     document.getElementById('repositories').innerHTML = repoList;
 }
