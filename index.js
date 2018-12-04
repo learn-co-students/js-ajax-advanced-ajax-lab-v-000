ab06bccb74f01a91e3a0401e29ca8427523a5b25
stazman <cadistasio@gmail.com>
2018-12-03 20:38:32 -0600

    Done.

--- index.js
+++ index.js
@@ -7,8 +7,18 @@ function getRepositories() {
 
 function showRepositories(event, data) {
     const repos = JSON.parse(this.responseText);
-    const repoList = `<ul>${repos
-        .map(r => '<li><a href="' + r.html_url + '">' + r.name + '</a></li>')
-        .join('')}</ul>`;
+    const repoList = '<ul>' +
+    repos
+      .map(r => {
+        return `
+          <li>
+            <h2><a href="${r.html_url}">${r.name}</a></h2>
+            <p>Watchers: ${r.watchers_count}</p>
+            <p>Forks: ${r.forks_count}</p>
+            <p>Issues: ${r.open_issues_count}</p>
+          </li>`;
+      })
+      .join('') +
+    '</ul>';
     document.getElementById('repositories').innerHTML = repoList;
 }
