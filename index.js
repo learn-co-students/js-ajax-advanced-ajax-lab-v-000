9a9c7ef7c4e147d76fc83857039d7ca792e2909e
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-12-30 14:09:25 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+  req.addEventListener('load', showRepositories);
+}
+
+function showRepositories(event, data) {
+  const repos = JSON.parse(this.responseText);
+  const src = document.getElementById('repository-template').innerHTML;
+  const template = Handlebars.compile(src);
+  const repoList = template(repos);
+  document.getElementById('repositories').innerHTML = repoList;
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  Handlebars.registerPartial(
+    'authorPartial',
+    document.getElementById('author-partial-template').innerHTML
+  );
+});
