5c6bd3261495818717400e2091cbb0f8f1daffd3
Alexandra Fren <alexandra.fren@gmail.com>
2019-01-11 17:46:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function getRepositories() {
+  const req= new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
+
+function showRepositories(event, data) {
+  const repos = JSON.parse(this.responseText);
+  const repoList = '<ul>${repos}'
+}
