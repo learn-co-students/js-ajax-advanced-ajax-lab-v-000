498b31aac7df885c9b7b55789bab762316c1ec17
epic0448 <samuelogundiran@Yahoo.com>
2019-02-02 16:30:06 -0500

    Done.

--- index.js
+++ index.js
@@ -10,10 +10,18 @@ function getRepositories() {
 function showRepositories(event, data){
     const repos = JSON.parse(this.responseText);
     // const repoList = `<ul>${repos.map(r => '<li><a href="' + r.html_url + '">' + r.name + '</a></li>')
-    // .join('')}</ul>`; It giving to us in the Handlebars
+    // .join('')}</ul>`; It giving to us in the Handle
 
     const template = Handlebars.compile(src);
     const repoList = template(repos)
 
     document.getElementById('repositories').innerHTML = repoList;
 }
+
+ document.addEventListener('DOMContentLoaded', 
+    function(event){
+        Handlebars.registerPartial(
+            'authorPartial',
+            document.getElementById('author-partial-template').innerHTML
+        );
+    })
